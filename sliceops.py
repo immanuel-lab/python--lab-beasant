a800a0d0e926f0229f2d79b47270c11eccd9318f
Immanuel <immanuelrajmohan3@gmail.com>
2025-04-29 09:53:59 +0530

    modified

--- sliceops.py
+++ sliceops.py
@@ -1,30 +1,27 @@
+# my_list=[0,1,2,3,4,5,6,7,8,9]
+            #0,1,2,3,4,5,6,7,8,9      index from start
+        #-10,-9,-8,-7,-6,-5,-4,-3-2-1       index from end
+# [start:end:step]
 
-# : is from a to b
-# is a step operator
+# print(my_list[5]) 
+# print(my_list[-5]) 
 
+# print(my_list[5:])
+# print(my_list[-5:])
 
-# lst=[1,2,3,4,5,6,7,8,9,12,112]
-# print(lst[1:3])
+# print(my_list[1:5])
+# print(my_list[1:-4])
+# print(my_list[-5:-1])
 
-# lst=[1,2,3,4,5,6,7,8,9,12,112]
-# print(lst[::-1])
-
-# lst=[1,2,3,4,5,6,7,8,9,12,112]
-# print(lst[-5::-1])
-
-
-# lst=[1,2,3,4,5,6,7,8,9,12,112]
-# print(lst[1::3])
-
-
-# this counts from back
-# lst=[1,2,3,4,5,6,7,8,9,12,112]
-# print(lst[2:-3])
+# print(my_list[0:9:3])
 
 
-# lst=[1,2,3,4,5,6,7,8,9,12,112]
-# print(lst[2::-3])
+# my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(my_list[-1:-5:-1])  # Output: [9, 8, 7, 6]
 
+             
 
-# lst=[1,2,3,4,5,6,7,8,9,12,112]
-# print(lst[5::-3])
+# my_list=[0,1,2,3,4,5,6,7,8,9]
+
+# print(my_list[::-1])
+# print(my_list[-1::-2])
